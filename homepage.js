5d3e9e5cabb9831b96b9f2b2588b1f5ca4de70d1
CostaRebecca <rebeccacosta1310@gmail.com>
2023-03-24 15:32:57 +0100

    commit

--- homepage.js
+++ homepage.js
@@ -26,7 +26,7 @@ const createAlbum = async () => {
         col.className = "container p-3 mb-3 mt-5"
         col.innerHTML = `
        
-        <div class="row d-flex py-4">
+        <div class="row d-flex py-4" style="background: linear-gradient(20deg, rgba(0,0,0,1) 0%, rgba(45,45,45,1) 61%, rgba(47,47,47,1) 67%, rgba(52,52,52,1) 93%);" >
         <div class="col-3 ps-4 d-flex align-items-center">
           <img class=" d-flex align-items-center w-100 h-xxl-100 h-xl-100 h-lg-75 mt-lg-3 mt-md-5" style="object-fit: cover;" src="${selectedAlbum.album.cover_big}" alt="">
         </div>
@@ -51,9 +51,9 @@ const createAlbum = async () => {
 
         row.appendChild(col);
 
-      
 
-        
+
+
 
 
 
@@ -61,71 +61,148 @@ const createAlbum = async () => {
         console.log(error);
     }
 
-} 
+}
 
 //FUNZIONE CARD PIU PICCOLE 
 
-const createCard = async () => {
+const createCard = async (selectedAlbumCard) => {
     const row = document.getElementById("cardHome");
+    const col = document.createElement("div");
+    col.className = "col-md-5 col-lg-3 col-xl-3 col-xxl-2 mb-4";
+    col.innerHTML = `
+    <div class="row ps-0 pe-0 ms-0 rounded rounded-1 d-flex gap-lg-5 g-xl-0 gap-xl-0 gap-xxl w-100" style="background-color: #2d2d2d; min-width: 200px; max-height: 75px">
+      <div class="row w-100 h-100">
+        <div class="col-md-2 col-lg-2 col-xl-2 p-0 d-flex me-auto">
+          <img class="p-0 rounded rounded-1" width="75px" src="${selectedAlbumCard.album.cover_medium}" alt="">
+        </div>
+        <div class="col-md-8 col-lg-8 col-xl-8 text-light d-flex justify-content-end align-items-center fw-semibold ps-5 ms-auto" style="max-height: 75px">
+        <a href="#" class="text-decoration-none text-light ps-5" style="max-height: 75px; min-width: 135px">
+        ${selectedAlbumCard.album.title}
+        </a>
+        </div>
+      </div>
+    </div>
+    `;
+
+    row.appendChild(col);
+};
+
+const createCards = async () => {
+    const row = document.getElementById("cardHome");
+    row.innerHTML = '';
+
     try {
         const response = await fetch("https://striveschool-api.herokuapp.com/api/deezer/search?q=a")
-
         const selectedCard = await response.json();
-
         console.log(selectedCard);
 
-        const randomIndexCard = Math.floor(Math.random() * selectedCard.data.length);
-        console.log("Random Index: ", randomIndexCard);
+        const shuffledData = shuffle(selectedCard.data);
 
-        const selectedAlbumCard = selectedCard.data[randomIndexCard];
-        
-
-        
-        let rowww = document.getElementById("cardHome");
-        rowww.innerHTML = '';
-         
-        const col = document.createElement("div")
-        col.className = "row row-cols-md-2 row-cols-lg-3 row-cols-xl-3 gap-3 mb-4"
-        col.innerHTML = `
-        <div class="row ps-0 pe-0 ms-0 rounded rounded-1 d-flex flex-column">
-        <div class="col-md-4 col-lg-3 p-0">
-          <img class="p-0 rounded rounded-1"  src="${selectedAlbumCard.album.cover_medium}" alt="">
-        </div>
-        <div class="col text-light d-flex align-items-center pe-2">
-          <p class=" fw-semibold fs-3">${selectedAlbumCard.album.title}</p>
-
-        </div>
-      </div>
-      </div>
-
-        `;
-
-        row.appendChild(col);
-
-      
-        
-
-
-
+        for (let i = 0; i < shuffledData.length; i++) {
+            await createCard(shuffledData[i]);
+        }
     } catch (error) {
         console.log(error);
     }
+};
 
-} 
-
-
-
-
-window.onload = function () {
-    
-    
-    createAlbum()
-    createCard()
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
 }
 
 
 
+/* +++++++++      +++++++   Carousel  ++++++++++     +++++++*/
 
+const createCardCarousel = async (selectedAlbumCards) => {
+    const row = document.getElementById("pageCards");
+    const col = document.createElement("div");
+    col.className = "col-md-3 col-lg-3 col-xl-2 ps-1 pb-2 gap-5 d-flex align-items-center justify-content-center";
+    col.innerHTML = `
+   
+    <div class="card d-flex align-items-center justify-content-start bg-dark">
+      <div class="p-1 rounded">
+        <img src="${selectedAlbumCards.album.cover_medium}" class="card-img-top px-3 pt-3 rounded rounded-4"
+          width="50px" alt="...">
+      </div>
+      <div class="card-body d-flex flex-column p-0 py-3   justify-content-start">
+        <h5 class="card-title text-light">${selectedAlbumCards.album.title}</h5>
+        <p class="card-text text-light opacity-25">${selectedAlbumCards.artist.name}</p>
+      </div>
+    </div>                    
+  `;
+
+    row.appendChild(col);
+
+};
+
+const createCardCarousel2 = async (selectedAlbumCards) => {
+  const row = document.getElementById("carosello2");
+  const col = document.createElement("div");
+  col.className = "col-md-3 col-lg-3 col-xl-2 ps-1 pb-2 gap-5 d-flex align-items-center justify-content-center";
+  col.innerHTML = `
+    <div class="card d-flex align-items-center justify-content-start bg-dark">
+      <div class="p-1 rounded">
+        <img src="${selectedAlbumCards.album.cover_medium}" class="card-img-top px-3 pt-3 rounded rounded-4"
+          width="50px" alt="...">
+      </div>
+      <div class="card-body d-flex flex-column p-0 py-3   justify-content-start">
+        <h5 class="card-title text-light">${selectedAlbumCards.album.title}</h5>
+        <p class="card-text text-light opacity-25">${selectedAlbumCards.artist.name}</p>
+      </div>
+    </div> 
+  
+`;
+
+  row.appendChild(col);
+
+};
+
+
+
+const createCardsCarousel = async () => {
+    const row = document.getElementById("pageCards");
+    row.innerHTML = '';
+    const row2 = document.getElementById("carosello2");
+    row.innerHTML = '';
+
+    try {
+        const response = await fetch("https://striveschool-api.herokuapp.com/api/deezer/search?q=a")
+        const selectedCardCarousel = await response.json();
+        console.log(selectedCardCarousel);
+
+        const shuffledData = shuffle(selectedCardCarousel.data);
+
+        for (let i = 0; i < shuffledData.length; i++) {
+            await createCardCarousel(shuffledData[i]);
+            await createCardCarousel2(shuffledData[i]);
+        }
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
+
+window.onload = function () {
+
+    createAlbum();
+    createCard();
+    createCards();
+    createCardCarousel();
+    createCardsCarousel();
+
+}
 
 
 
